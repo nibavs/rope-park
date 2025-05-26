b961a9cd7bbae3e13974ec38e8d261108ed9a1fe
Nikita Bavs <nbavsunovsky@gmail.com>
2025-05-26 12:39:37 +0700

    Initial commit

---
+++ rope-park/src/main/java/com/nibavs/rope_park/RopeParkApplication.java
@@ -0,0 +1,13 @@
+package com.nibavs.rope_park;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RopeParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RopeParkApplication.class, args);
+	}
+
+}
---
+++ rope-park/src/test/java/com/nibavs/rope_park/RopeParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nibavs.rope_park;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RopeParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
